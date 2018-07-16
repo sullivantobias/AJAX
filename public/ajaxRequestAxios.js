3a305674d39c00c73ee6bece912cb4831513b76c
Sullivan Tobias <githubst@gmail.com>
2018-07-16 10:27:09 +0200

    Add comments for AXIOS request

--- public/ajaxRequestAxios.js
+++ public/ajaxRequestAxios.js
@@ -3,12 +3,20 @@
  * Dependencies : AXIOS
  */
 
+ /** Just a simple request using AXIOS */
 $("#showInfosAxios").on("click", () => {
   axios({
+
+    /** We define GET Method */
     method: "GET",
+    /** We specify URL to reach the targeted file */
     url: "public/data/people.json"
   })
+    /** We use promises with the purpose of treating retrieved data */
     .then(request => {
+      /** We do the treatement here
+       * This treatement is only done if the request is right
+       */
       const prefix = request.data.First;
       const name = "<b>Name</b> : " + prefix.name + "</br>";
       const surname = "<b>Surname</b> : " + prefix.surname + "</br>";
@@ -18,6 +26,7 @@ $("#showInfosAxios").on("click", () => {
         "<p>" + name + " " + surname + " " + country + " " + activity + "</p>";
     })
     .catch(() => {
+      /** With catch you can "catch" the error if the request failed */
       console.error("AJAX Issues detected");
     });
 });
