859157804b4b69be46c1503b793ae280cf54b8ea
Tobias <githubst@gmail.com>
2018-07-18 23:29:23 +0200

    Refactor code with Google Standard

--- public/ajaxRequest.js
+++ public/ajaxRequest.js
@@ -4,36 +4,47 @@
  */
 
 $(function() {
-  $("#showInfos").on("click", () => {
-    $.ajax({
-      type: "GET",
-      url: "public/data/people.json",
-      success: data => {
-        for (firstKey in data) {
-          let prefixFormat = data[firstKey];
+    $('#showInfos').on('click', () => {
+        $.ajax({
+            type: 'GET',
+            url: 'public/data/people.json',
+            success: (data) => {
+                for (let firstKey in data) {
+                    if (data) {
+                        let prefixFormat = data[firstKey];
 
-          console.log(prefixFormat);
-          const name = "<b>Name</b> : " + prefixFormat.name + "</br>";
-          const surname = "<b>Surname</b> : " + prefixFormat.surname + "</br>";
-          const country = "<b>country</b> : " + prefixFormat.country + "</br>";
-          const activity =
-            "<b>activity</b> : " + prefixFormat.activity + "</br>";
-          $("#infos").append(
-            "<p>" +
-              name +
-              " " +
-              surname +
-              " " +
-              country +
-              " " +
-              activity +
-              "</p>"
-          );
-        }
-      },
-      error: () => {
-        console.log("Wrong");
-      }
+                        // console.log(prefixFormat);
+                        const name =
+                            '<b>Name</b> : ' + prefixFormat.name + '</br>';
+                        const surname =
+                            '<b>Surname</b> : ' +
+                            prefixFormat.surname +
+                            '</br>';
+                        const country =
+                            '<b>country</b> : ' +
+                            prefixFormat.country +
+                            '</br>';
+                        const activity =
+                            '<b>activity</b> : ' +
+                            prefixFormat.activity +
+                            '</br>';
+                        $('#infos').append(
+                            '<p>' +
+                                name +
+                                ' ' +
+                                surname +
+                                ' ' +
+                                country +
+                                ' ' +
+                                activity +
+                                '</p>'
+                        );
+                    }
+                }
+            },
+            error: () => {
+                // console.log('Wrong');
+            },
+        });
     });
-  });
 });
